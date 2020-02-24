abf703b504191d5fd419738c740890949fff0b4d
aagbh <aagbh@allstate.com>
2020-02-23 22:15:02 -0600

    Changes to support PCF deployment

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)             # create an app instance
 def hello(name):                      # call method hello
     return "Hello "+name.upper()        # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
-    app.run(debug=True)                     # run the flask app
+    app.run(host='0.0.0.0',port='8080')                     # run the flask app
